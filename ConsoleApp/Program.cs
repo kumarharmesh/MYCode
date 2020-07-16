c31ab7051a508659df8370cc84ffb2dd709364ff
Harmesh <kumarharmesh@gmail.com>
2020-07-16 20:32:34 +0530

    changes made

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,6 +8,10 @@ namespace ConsoleApp
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine(DateManip.Leap(2000));
+
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                Console.WriteLine("Stashed changes.");
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~            
         }
     }
 }
--- maths.cs
+++
@@ -1,8 +0,0 @@
-using System;
-namespace ConsoleApp
-{
-  public enum Months
-  {
-     JANUARY=1,FEBRUARY,MARCH,APRIL,MAY,JUNE,JULY,AUGUST,SEPTEMBER,OCTOBER,NOVEMBER,DECEMBER
-  }
-}
